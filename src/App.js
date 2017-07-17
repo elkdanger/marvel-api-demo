2bd11dbb278c7d7f159c9801453e30dd36abbc51
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-07-18 00:00:54 +0100

    Basic theme styles and app component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="page-header">Marvel comic browser</h1>
+      </div>
+    )
+  }
+}
