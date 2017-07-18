314d80a7f7d0f87d4f63d3233f7c6a58ea674663
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-07-18 11:44:55 +0100

    Styled up a quick header with logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import * as React from 'react'
+import logo from './marvel.png'
+import './App.css'
 
 export class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1 className="page-header">Marvel comic browser</h1>
+        <div className="row header">
+          <div className="col-sm-8">
+            <img src={logo} alt="Marvel" />
+            <h1>
+              <span className="visually-hidden">Marvel</span> character browser
+            </h1>
+          </div>
+          <div className="col-sm-4" />
+        </div>
       </div>
     )
   }
