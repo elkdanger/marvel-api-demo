c717994ec1fce8f240193d7db4611f0f69efdc2a
Steve Hobbs <steve.hobbs@digital.hmrc.gov.uk>
2017-07-19 23:35:22 +0100

    Intalled Masonry to display image grid

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,19 +1,23 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
+import Masonry from 'react-masonry-component'
 
 const mapStateToProps = state => ({
   data: state.searchResults
 })
 
+const masonryOptions = {}
+
+const thumbUrl = item => {
+  return `${item.thumbnail.path}/portrait_xlarge.${item.thumbnail.extension}`
+}
+
 export class SearchResultsComponent extends React.Component {
   render() {
-    const thumbUrl = item =>
-      `${item.thumbnail.path}/portrait_fantastic.${item.thumbnail.extension}`
-
     return (
-      <div className="grid">
+      <Masonry className="search-results">
         {this.props.data.map(d => <img src={thumbUrl(d)} />)}
-      </div>
+      </Masonry>
     )
   }
 }
